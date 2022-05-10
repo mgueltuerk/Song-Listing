2609fcaaadd6d7318713430af4e5125c367fef2d
Murat Gültürk <m.gueltuerk@gmx.de>
2022-05-10 15:20:45 +0200

    Initial Commit

---
+++ Song Listing/Song_ListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Song_ListingApp.swift
+//  Song Listing
+//
+//  Created by Murat Gültürk on 10.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Song_ListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
